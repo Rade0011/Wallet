c419dab87e7a0be6f2edf1f213293823facf5bea
Tima <140940996+Rade0011@users.noreply.github.com>
2024-02-07 17:00:51 +0300

    add registration and validation

--- wallet/src/user/dto/create-user.dto.ts
+++ wallet/src/user/dto/create-user.dto.ts
@@ -1,6 +1,10 @@
+import { IsEmail, MinLength } from "class-validator";
 
 
 export class CreateUserDto {
+    @IsEmail()
     email: string;
+
+    @MinLength(6, {message: 'Password must be more 6 symbols'})
     password: string;
 }
